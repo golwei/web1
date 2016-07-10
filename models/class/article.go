5558a9d9ac7d9eb93bd03b71aeca1dc20ec00182
golwei <lizhiweimail@qq.com>
2016-07-10 09:42:00 +0800

    增加 article model

---
+++ models/class/article.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+)
+
+type Article struct {
+	Id      int
+	Title   string
+	Content string `orm:"type(text)"`
+	Author  *User
+	Replys  int
+	Views   int
+	Tag     string
+	Time    time.Time `orm:"auto_now_add"`
+}
