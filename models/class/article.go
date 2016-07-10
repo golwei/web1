1edcfc942d1a586dff40b64dce429b11eb8c00c0
golwei <lizhiweimail@qq.com>
2016-07-10 10:33:26 +0800

    行号错误

--- models/class/article.go
+++ models/class/article.go
@@ -17,19 +17,16 @@ import (
 //	Time    time.Time `orm:"auto_now_add"`
 //}
 
+type Article struct {
+	Id      int
+	Title   string
+	Content string `orm:"type(text)"`
+	Author  *User  `orm:"rel(fk);size(30)"`
 
-  7 type Article struct {
-  8         Id      int
-  9         Title   string
- 10         Content string `orm:"type(text)"`
- 11         Author  *User  `orm:"rel(fk);size(30)"`
- 12 
- 13         NumReplys int
- 14         NumViews  int
- 15 
- 16 
- 17         Time time.Time `orm:"auto_now_add;type(datetime)"`
- 18 
- 19         Defunct bool
- 20 }
- 21 
+	NumReplys int
+	NumViews  int
+
+	Time time.Time `orm:"auto_now_add;type(datetime)"`
+
+	Defunct bool
+}
