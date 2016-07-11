fbe5964c353d4056c1592f105c3b62fa70d389e3
golwei <lizhiweimail@qq.com>
2016-07-11 09:17:25 +0800

    views

--- controllers/default.go
+++ controllers/default.go
@@ -4,6 +4,7 @@ import (
 	"github.com/astaxie/beego"
 )
 
+//Main{}
 type MainController struct {
 	beego.Controller
 }
@@ -12,4 +13,13 @@ func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+}
+
+// User{}
+type UserController struct {
+	beego.Controller
+}
+
+func (c *UserController) Profile() {
+	c.TplName = "user/profile.html"
 }
